44fca03035d64a780e59bfaca3a76de9813e1176
Takemoto <victor_takemoto@outlook.com>
2023-12-18 11:57:25 -0300

    Implementando Salvamento em banco

--- SmartHint.WebApp/obj/Debug/net8.0/SmartHint.WebApp.AssemblyInfo.cs
+++ SmartHint.WebApp/obj/Debug/net8.0/SmartHint.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHint.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+688a05f54c0887ecea5b3691c73359e24d1a5d22")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHint.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHint.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
